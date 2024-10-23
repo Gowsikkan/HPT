3d15ea8b4144f4a10f694867dc87b11e0ccbc7cc
Gowsikkan <66643168+Gowsikkan@users.noreply.github.com>
2024-10-24 01:33:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,3 +46,47 @@ def download_zip(url):
         else:
             
             print("No <a> tag found in list item.")
+import pandas as pd
+
+# Creating the dataframe
+data = {'filename': ['file1']*12,
+        'page': list(range(1, 13)),
+        'class': [[] for _ in range(12)],
+        'flag': [1, 0, 0, 0, 0, -1, 0, 0, 1, -1, 1, 0]}
+
+df = pd.DataFrame(data)
+
+def process_flags(df):
+    i = 0
+    n = len(df)
+    
+    while i < n:
+        if df.loc[i, 'flag'] == 1:
+            # Search for the next +1 or -1 within 30 rows
+            found = False
+            for j in range(i + 1, min(i + 31, n)):
+                if df.loc[j, 'flag'] in [-1, 1]:
+                    # Append 'pn' to the rows up to the found +1 or -1
+                    df.loc[i+1:j, 'class'] = df.loc[i+1:j, 'class'].apply(lambda x: x + ['pn'])
+                    i = j  # Move to the found row
+                    found = True
+                    break
+            if not found:
+                # If no -1 or +1 found, append 'pn' to the next 5 rows
+                df.loc[i+1:min(i+6, n), 'class'] = df.loc[i+1:min(i+6, n), 'class'].apply(lambda x: x + ['pn'])
+                i += 6  # Skip the next 6 rows
+        elif df.loc[i, 'flag'] == -1:
+            # Remove 'pn' from rows until the next +1
+            for j in range(i + 1, n):
+                if df.loc[j, 'flag'] == 1:
+                    i = j  # Move to the found +1
+                    break
+                df.loc[j, 'class'] = []
+        else:
+            i += 1  # Continue to the next row if flag is 0 or any other value
+    
+    return df
+
+# Process the dataframe
+df_processed = process_flags(df)
+df_processed
